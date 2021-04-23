8aa380d93c0475d6c2e71b278254318d679fd925
Manuel-Arias0901 <arias.rangel.jose.manuel@gmail.com>
2021-04-23 13:02:35 -0500

    se agrega overflow al pdf

--- app/reportes/product_pdf.rb
+++ app/reportes/product_pdf.rb
@@ -28,43 +28,17 @@ class ProductPdf
                 Product.all.each do |x|
                     text_box x.id.to_s, at: [20, cursor]
                     text_box x.name, at: [70, cursor]
-                    text_box x.description, at: [200, cursor]
-                    text_box x.price.to_s, at: [400, cursor]
+                    text = text_box x.description, at: [200, cursor], width: 180, height: 15, overflow: :truncate
+                    text_box x.price.to_s, align: :center, width: 50, at: [400, cursor]
                     text_box x.tipo.name, at: [480, cursor]
                     move_down 15
+                    while text.length > 0  do
+                        text = text_box text, at: [200, cursor], width: 180, height: 15, overflow: :truncate
+                        move_down 15
+                    end 
+                    move_down 10
                 end
-                   
-                #define_grid(columns: 5, rows: 8, gutter: 11)
-                #grid([1, 0], [7, 0]).bounding_box do
-                #    text 'id', align: :center, size: 14, style: :bold
-                #    Product.all.each do |i|
-                #        text i.id.to_s, align: :center
-                #    end
-                #end
-                #grid([1, 2], [7, 0]).bounding_box do
-                #    text 'Nombre', align: :center, size: 14, style: :bold
-                #    Product.all.each do |n|
-                #        text n.name, align: :center
-                #    end
-                #end
-                #grid([1, 3], [7, 2]).bounding_box do
-                #    text 'Descripcion', align: :center, size: 14, style: :bold
-                #    Product.all.each do |d|
-                #        text d.description, align: :center
-                #    end
-                #end
-                #grid([1, 4], [7, 3]).bounding_box do
-                #    text 'Precio', align: :center, size: 14, style: :bold
-                #    Product.all.each do |p|
-                #        text p.price.to_s, align: :center
-                #    end
-                #end
-                #grid([1, 5], [7, 3]).bounding_box do
-                #    text 'Tipo', align: :center, size: 14, style: :bold
-                #    Product.all.each do |t|
-                #        text t.tipo.name, align: :center
-                #    end
-                #end  
+               
             end
         end
         pdf
